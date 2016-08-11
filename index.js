a728d0465ebcc2cd2f7cb0734ada7431b5c55d9a
maksad <m90x@mail.ru>
2016-08-11 19:24:40 +0300

    Convert yyyy-mm-dd from es6 to es5

--- index.js
+++ index.js
@@ -3,22 +3,22 @@
 module.exports = function (date) {
   date = date || new Date()
 
-  const year = date.getFullYear()
-  const month = twoDigit(date.getMonth() + 1)
-  const day = twoDigit(date.getDate())
+  var year = date.getFullYear()
+  var month = twoDigit(date.getMonth() + 1)
+  var day = twoDigit(date.getDate())
 
-  return `${year}-${month}-${day}`
+  return year + '-' + month + '-' + day
 }
 
 module.exports.withTime = function (datetime) {
   datetime = datetime || new Date()
 
-  const date = this(datetime)
-  const hour = twoDigit(datetime.getHours())
-  const minutes = twoDigit(datetime.getMinutes())
-  const seconds = twoDigit(datetime.getSeconds())
+  var date = this(datetime)
+  var hour = twoDigit(datetime.getHours())
+  var minutes = twoDigit(datetime.getMinutes())
+  var seconds = twoDigit(datetime.getSeconds())
 
-  return `${date} ${hour}:${minutes}:${seconds}`
+  return date + ' ' + hour + ':' + minutes + ':' + seconds
 }
 
 function twoDigit (n) {
